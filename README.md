25756cc204afd321e3abf2e8d13b8c3affee1895
Raabb Ajam <raabbajam@gmail.com>
2016-05-12 19:50:49 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,70 @@
+<big><h1 align="center">raabbajam-calculator</h1></big>
+
+<p align="center">
+  <a href="https://npmjs.org/package/raabbajam-calculator">
+    <img src="https://img.shields.io/npm/v/raabbajam-calculator.svg?style=flat-square"
+         alt="NPM Version">
+  </a>
+
+  <a href="https://coveralls.io/r/raabbajam/raabbajam-calculator">
+    <img src="https://img.shields.io/coveralls/raabbajam/raabbajam-calculator.svg?style=flat-square"
+         alt="Coverage Status">
+  </a>
+
+  <a href="https://travis-ci.org/raabbajam/raabbajam-calculator">
+    <img src="https://img.shields.io/travis/raabbajam/raabbajam-calculator.svg?style=flat-square"
+         alt="Build Status">
+  </a>
+
+  <a href="https://npmjs.org/package/raabbajam-calculator">
+    <img src="http://img.shields.io/npm/dm/raabbajam-calculator.svg?style=flat-square"
+         alt="Downloads">
+  </a>
+
+  <a href="https://david-dm.org/raabbajam/raabbajam-calculator.svg">
+    <img src="https://david-dm.org/raabbajam/raabbajam-calculator.svg?style=flat-square"
+         alt="Dependency Status">
+  </a>
+
+  <a href="https://github.com/raabbajam/raabbajam-calculator/blob/master/LICENSE">
+    <img src="https://img.shields.io/npm/l/raabbajam-calculator.svg?style=flat-square"
+         alt="License">
+  </a>
+</p>
+
+<p align="center"><big>
+Calculate infix expression of number and operator
+</big></p>
+
+
+## Install
+
+```sh
+npm i -D raabbajam-calculator
+```
+
+## Usage
+
+```js
+import raabbajamCalculator from "raabbajam-calculator"
+
+raabbajamCalculator() // true
+```
+
+## License
+
+MIT © [Raabb Ajam](http://github.com/raabbajam)
+
+[npm-url]: https://npmjs.org/package/raabbajam-calculator
+[npm-image]: https://img.shields.io/npm/v/raabbajam-calculator.svg?style=flat-square
+
+[travis-url]: https://travis-ci.org/raabbajam/raabbajam-calculator
+[travis-image]: https://img.shields.io/travis/raabbajam/raabbajam-calculator.svg?style=flat-square
+
+[coveralls-url]: https://coveralls.io/r/raabbajam/raabbajam-calculator
+[coveralls-image]: https://img.shields.io/coveralls/raabbajam/raabbajam-calculator.svg?style=flat-square
+
+[depstat-url]: https://david-dm.org/raabbajam/raabbajam-calculator
+[depstat-image]: https://david-dm.org/raabbajam/raabbajam-calculator.svg?style=flat-square
+
+[download-badge]: http://img.shields.io/npm/dm/raabbajam-calculator.svg?style=flat-square
---
+++ src/index.js
@@ -0,0 +1,166 @@
+export default function() {
+  return true;
+}
+
+export function tokenize(text) {
+  const tokens = [];
+  let number = null;
+  text.match(/\S/g).forEach((token) => {
+    if (isNumber(token)) {
+      number = `${number || ''}${String(token)}`;
+    } else {
+      if (number) {
+        tokens.push(number);
+      }
+      tokens.push(token);
+      number = null;
+    }
+  });
+  return tokens;
+}
+
+export function RPNize(tokens) {
+  const stack = [];
+  let output = [];
+  tokens.forEach((token) => {
+    if (isNumber(token)) {
+      output.push(token);
+    } else if (isFunction(token)) {
+      stack.push(token);
+    } else if (isOperator(token)) {
+      const operator1 = token;
+      while (tail(stack) && isOperator(tail(stack))) {
+        const operator2 = tail(stack);
+        if (isLeftAssociative(operator1) && isLtePrecedence(operator1, operator2) ||
+          isRightAssociative(operator1) && isLtPrecedence(operator1, operator2)
+        ) {
+          output.push(stack.pop());
+        } else {
+          break;
+        }
+      }
+      stack.push(token);
+    } else if (isRightParentheses(token)) {
+      while (tail(stack) && !isLeftParentheses(tail(stack))) {
+        output.push(stack.pop());
+      }
+
+      if (!isLeftParentheses(tail(stack))) {
+        throw new Error('Mismatch parentheses');
+      }
+      stack.pop();
+    }
+  });
+  output = output.concat(stack.reverse());
+  return output;
+}
+
+export function evaluate(tokens) {
+  const stack = [];
+  tokens.forEach((token) => {
+    if (isNumber(token)) {
+      stack.push(token);
+    } else if (isOperator(token)) {
+      const operator = token;
+      const operand2 = stack.pop();
+      const operand1 = stack.pop();
+      const result = execute(operator, operand1, operand2);
+      stack.push(result);
+    }
+  });
+  return stack.pop();
+}
+
+function isNumber(text) {
+  return /\d/.test(text);
+}
+
+function isFunction(token) {
+  return /\(/.test(token);
+}
+
+function isLeftParentheses(token) {
+  return /\(/.test(token);
+}
+
+function isRightParentheses(token) {
+  return /\)/.test(token);
+}
+
+function isOperator(token) {
+  return /[\+\-\*\/\^]/.test(token);
+}
+
+function tail(stack) {
+  return stack[stack.length - 1];
+}
+
+function isLeftAssociative(operator) {
+  return /[\+\-\*\/]/.test(operator);
+}
+
+function isRightAssociative(operator) {
+  return /\^/.test(operator);
+}
+
+function isLtePrecedence(operator1, operator2) {
+  const operatorPrecedence1 = getPrecedence(operator1);
+  const operatorPrecedence2 = getPrecedence(operator2);
+  return operatorPrecedence1 <= operatorPrecedence2;
+}
+
+function isLtPrecedence(operator1, operator2) {
+  const operatorPrecedence1 = getPrecedence(operator1);
+  const operatorPrecedence2 = getPrecedence(operator2);
+  return operatorPrecedence1 < operatorPrecedence2;
+}
+
+function getPrecedence(operator) {
+  let precedence = 0;
+  switch (operator) {
+    case '^':
+      precedence = 4;
+      break;
+    case '*':
+      precedence = 3;
+      break;
+    case '/':
+      precedence = 3;
+      break;
+    case '+':
+      precedence = 2;
+      break;
+    case '-':
+      precedence = 2;
+      break;
+    default:
+      precedence = 0;
+  }
+  return precedence;
+}
+
+function execute(operator, operand1, operand2) {
+  operand1 = Number(operand1);
+  operand2 = Number(operand2);
+  let result = 0;
+  switch (operator) {
+    case '+':
+      result = operand1 + operand2;
+      break;
+    case '-':
+      result = operand1 - operand2;
+      break;
+    case '*':
+      result = operand1 * operand2;
+      break;
+    case '/':
+      result = operand1 / operand2;
+      break;
+    case '^':
+      result = Math.pow(operand1, operand2);
+      break;
+    default:
+      result = 0;
+  }
+  return result;
+}
